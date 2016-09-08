eb709fcdcd48f50b388ec5fd276dd4a3097f1988
Mark Kenny <mkenny@plus.net>
2016-09-08 16:31:27 +0100

    fixup! First commit

--- scanner.php
+++ scanner.php
@@ -39,92 +39,92 @@ $photosDirectory = "$notesDirectory/Pictures";
 $outputDirectory = "$vesperDirectory/Output";
 
 function dateFormat($timeStamp) {
-    // Assume that the date is stored in GMT
-    return gmdate('Y-m-d\TH:i:s\Z', $timeStamp);
+	// Assume that the date is stored in GMT
+	return gmdate('Y-m-d\TH:i:s\Z', $timeStamp);
 }
 
 $files = scandir($notesDirectory);
 
 foreach ($files as $file) {
 
-    if ($file == '.' || $file == '..' || is_dir($file)) {
-        continue;
-    }
+	if ($file == '.' || $file == '..' || is_dir($file)) {
+		continue;
+	}
 
-    $note = file_get_contents("$notesDirectory/$file");
+	$note = file_get_contents("$notesDirectory/$file");
 
-    $regex = '/
-        (?:Picture: (.*)\n\n)?
-        Tags: (.*)\n\n
-        Created: (.*)\n
-        .*
-    /x';
+	$regex = '/
+		(?:Picture: (.*)\n\n)?
+		Tags: (.*)\n\n
+		Created: (.*)\n
+		.*
+	/x';
 
-    preg_match($regex, $note, $matches);
+	preg_match($regex, $note, $matches);
 
-    $postText = trim(str_replace($matches[0], '', $note));
+	$postText = trim(str_replace($matches[0], '', $note));
 
-    $postDate = $matches[3];
+	$postDate = $matches[3];
 
 	$postTags = explode(',', $matches[2]);
-    $postTags = array_unique($postTags);
-    $postTags = array_filter($postTags, function ($value) { return (strlen(trim($value)) > 0); });
+	$postTags = array_unique($postTags);
+	$postTags = array_filter($postTags, function ($value) { return (strlen(trim($value)) > 0); });
 
 	$postPhoto = trim($matches[1]);
 
-    $post = array();
+	$post = array();
 
-    if (is_array($postTags) && count($postTags) > 0) {
-        $post['tags'] = array_map(function ($value) { return trim($value); }, $postTags);
-    } else {
-        $post['tags'] = array();
-    }
+	if (is_array($postTags) && count($postTags) > 0) {
+		$post['tags'] = array_map(function ($value) { return trim($value); }, $postTags);
+	} else {
+		$post['tags'] = array();
+	}
 
-    $post['text'] = trim($postText);
+	$post['text'] = trim($postText);
 
-    if ($postPhoto) {
+	if ($postPhoto) {
 
-        $photoFilepath = "$photosDirectory/$postPhoto";
+		$photoFilepath = "$photosDirectory/$postPhoto";
 
-        $post['tags'][] = 'VesperPhoto';
-        $post['text'] .= "\n\nPhoto was: $postPhoto";
+		$post['tags'][] = 'VesperPhoto';
+		$post['text'] .= "\n\nPhoto was: $postPhoto";
 
-        $imageSize = getimagesize($photoFilepath);
-        $width = $imageSize[0];
-        $height = $imageSize[1];
-        $md5Hash = md5_file($photoFilepath);
-        $identifier = strtoupper(md5($photoFilepath . time()));
+		$imageSize = getimagesize($photoFilepath);
+		$width = $imageSize[0];
+		$height = $imageSize[1];
+		$md5Hash = md5_file($photoFilepath);
+		$identifier = strtoupper(md5($photoFilepath . time()));
 
-        copy($photoFilepath, "$outputDirectory/photos/$md5Hash.jpeg");
+		copy($photoFilepath, "$outputDirectory/photos/$md5Hash.jpeg");
 
-        $photos = array(
-            "fnumber"      => "(null)",
-            "orderInEntry" => 0,
-            "width"        => $width,
-            "type"         => "jpeg",
-            "identifier"   => $identifier,
-            "height"       => $height,
-            "md5"          => $md5Hash,
-            "focalLength"  => "(null)"
-            "date"         => dateFormat(filemtime($photoFilepath))
-        );
+		$photos = array(
+			"fnumber"	  => "(null)",
+			"orderInEntry" => 0,
+			"width"		=> $width,
+			"type"		 => "jpeg",
+			"identifier"   => $identifier,
+			"height"	   => $height,
+			"md5"		  => $md5Hash,
+			"focalLength"  => "(null)",
+			"date"		 => dateFormat(filemtime($photoFilepath))
+		);
 
-        $post['photos'] = array($photos);
-        $post['text'] = trim("![](dayone-moment://$identifier)\n\n" . $post['text']);
-    }
+		$post['photos'] = array($photos);
+		$post['text'] = trim("![](dayone-moment://$identifier)\n\n" . $post['text']);
+	}
 
-    $post['creationDate'] = dateFormat(strtotime($postDate));
-    $post['starred'] = false;
-    $post['timeZone'] = 'Europe/London';
-    $post['uuid'] = strtoupper(md5($post['text'] . time()));
+	$post['creationDate'] = dateFormat(strtotime($postDate));
+	$post['starred'] = false;
+	$post['timeZone'] = 'Europe/London';
+	$post['uuid'] = strtoupper(md5($post['text'] . time()));
 
-    $allPosts[] = $post;
+	$allPosts[] = $post;
 
 }
 
 $jsonOutput = array(
-    "metadata" => array("version" => "1.0"),
-    "entries" => $allPosts
+	"metadata" => array("version" => "1.0"),
+	"entries" => $allPosts
 );
 
 $allPostsJson = json_encode($jsonOutput);
